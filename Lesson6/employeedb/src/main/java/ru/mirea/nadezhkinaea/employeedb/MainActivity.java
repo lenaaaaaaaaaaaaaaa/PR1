235ad5b3d523566673619844705f0d9185e2db49
lenaaaaaaaaaaaaaaa <nadezhkinalena08@gmail.com>
2024-05-11 12:54:53 +0300

    six fix

--- Lesson6/employeedb/src/main/java/ru/mirea/nadezhkinaea/employeedb/MainActivity.java
+++ Lesson6/employeedb/src/main/java/ru/mirea/nadezhkinaea/employeedb/MainActivity.java
@@ -7,33 +7,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
-import ru.mirea.nadezhkinaea.employeedb.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-    private final String TAG = this.getClass().getSimpleName();
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        setContentView(R.layout.activity_main);
         AppDatabase db = App.getInstance().getDatabase();
         EmployeeDao employeeDao = db.employeeDao();
-
         Employee employee = new Employee();
-        employee.id = 1;
-        employee.name = "John Smith";
-        employee.salary = 10000;
+        employee.id = 2;
+        employee.name = "Человек-паук";
+        employee.salary = 20000;
 
+        // Запись сотрудников в базу
         employeeDao.insert(employee);
 
+        // Загрузка всех работников
         List<Employee> employees = employeeDao.getAll();
-        employee = employeeDao.getById(1);
-        employee.salary = 20000;
+
+        // Получение определенного работника с id = 2
+        employee = employeeDao.getById(2);
+
+        // Обновление полей объекта
+        employee.salary = 30000;
         employeeDao.update(employee);
-        Log.d(TAG, employee.name + " " + employee.salary);
+
+        Log.d("DB", employee.name + " " + employee.salary);
     }
 }
